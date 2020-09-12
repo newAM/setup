eb5472bc8fdf1a0ee026b9d0d78fcace2c5f7c78
Alex M <alexmgit@protonmail.com>
2020-09-12 12:23:33 -0700

    Add install_rust.sh

---
+++ install_rust.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+set -e
+
+if rustup --version;
+then
+    echo "rustup is already installed"
+else
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+fi
+
+echo ""
+
+declare -a components=(
+    llvm-tools-preview
+)
+
+for component in "${components[@]}"; do
+    rustup component add "$component"
+done
+
+echo ""
+
+declare -a crates=(
+    bandwhich
+    bat
+    bindgen
+    cargo-binutils
+    cargo-deadlinks
+    cargo-readme
+    cargo-spellcheck
+    cargo-tarpaulin
+    debcargo
+    du-dust
+    exa
+    gitoxide
+    gitui
+    just
+    tealdeer
+    ytop
+)
+
+for crate in "${crates[@]}"; do
+    cargo install "$crate"
+done
+
+cargo install tokei --features all
