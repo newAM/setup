2e0bd66e07b41adccede4bf8c877df2654286c20
Alex M <alexmgit@protonmail.com>
2020-09-12 19:50:50 -0700

    Add rust environment variable after install.

--- install_rust.sh
+++ install_rust.sh
@@ -6,6 +6,8 @@ then
     echo "rustup is already installed"
 else
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    # shellcheck disable=SC1090
+    source "$HOME/.cargo/env"
 fi
 
 echo ""
